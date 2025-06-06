6c1859f6dfe7550744ea9604bb78d43815a8f834
congMinh <minh.ptit.k6@gmail.com>
2025-06-06 23:50:47 +0700

    làm tag login P1

---
+++ asset/javascript/login.js
@@ -0,0 +1,19 @@
+import {divHeader} from "/asset/javascript/var.js"
+import {divNavBar} from "/asset/javascript/var.js"
+import {divMenu} from "/asset/javascript/var.js"
+
+
+
+
+window.addEventListener("scroll",()=>{
+    let value=window.scrollY>75;
+    if (value==true){
+        divHeader.classList.add("scroll");
+        divNavBar.classList.add("nav-bar-none");
+        divMenu.classList.add("box-menu");
+    }else{
+        divHeader.classList.remove("scroll");
+        divNavBar.classList.remove("nav-bar-none");
+        divMenu.classList.remove("box-menu");
+    }
+})

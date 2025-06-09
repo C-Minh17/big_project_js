145965c8ccb3152c86f333c5d0eacaa43515a2c7
congMinh <minh.ptit.k6@gmail.com>
2025-06-09 23:25:55 +0700

    làm phần signUp lưu tài khoản khi người dùng đăng kí

--- asset/javascript/login.js
+++ asset/javascript/login.js
@@ -28,5 +28,51 @@ document.querySelector("#upSignIn").addEventListener("click",()=>{
 })
 
 
+const upName=document.querySelector("#signUpName");
+const upEmail=document.querySelector("#signUpEmail");
+const upPhone=document.querySelector("#signUpPhone");
+const upPass=document.querySelector("#signUpPass");
+const upAlert=document.querySelector("#alertSignUp");
 
+let listAccount=[];
+class infoSignUp{
+    constructor(name,email,phone,password){
+        this.userName=name;
+        this.email=email;
+        this.phone=phone;
+        this.password=password;
+    }
+};
+listAccount.push(new infoSignUp("admin","admin@gmail.com","0999999999","admin123"))
+listAccount.concat(JSON.parse(localStorage.getItem("listAccount")));
+function ktraAccount(){
+    for (item of listAccount){
+        if (upName.value==item.userName || upEmail.value==item.email || upPhone.value==item.phone || upPass.value==item.password){
+            return false
+        }
+    };
+    return true
+};
+function resetValueUp(){
+    upName.value="";
+    upEmail.value="";
+    upPhone.value="";
+    upPass.value="";
+}
 
+document.querySelector("#upSignUp").addEventListener("click",()=>{
+    if (upName.value=="" || upEmail.value=="" || upPhone.value=="" || upPass.value==""){
+        upAlert.innerHTML="Vui lòng nhập đầy đủ thông tin yêu cầu"
+    }else{
+        if(ktraAccount){
+            console.log(listAccount);
+            listAccount.push(new infoSignUp(upName.value,upEmail.value,upPhone.value,upPass.value));
+            localStorage.listAccount=JSON.stringify(listAccount);
+            upAlert.innerHTML="Đăng kí thành công";
+            resetValueUp();
+        }else{
+            upAlert.innerHTML="Tài khoản đã tồn tại";
+        }
+        
+    }
+})

d1fef3d46890e158f77cda0af9e322b7b66bea83
congMinh <minh.ptit.k6@gmail.com>
2025-06-13 22:17:05 +0700

    làm form html trang item.html

---
+++ asset/javascript/item.js
@@ -0,0 +1,22 @@
+import {divHeader} from "/asset/javascript/var.js"
+import {divNavBar} from "/asset/javascript/var.js"
+import {divMenu} from "/asset/javascript/var.js"
+import {divButtonMapHotel} from "/asset/javascript/var.js"
+
+
+window.addEventListener("scroll",()=>{
+    let value=window.scrollY>75;
+    if (value==true){
+        divHeader.classList.add("scroll");
+        divNavBar.classList.add("nav-bar-none");
+        divMenu.classList.add("box-menu");
+    }else{
+        divHeader.classList.remove("scroll");
+        divNavBar.classList.remove("nav-bar-none");
+        divMenu.classList.remove("box-menu");
+    }
+})
+s
+divButtonMapHotel.addEventListener("click",()=>{
+    document.querySelector("#mapHotel").classList.toggle("map-tour-none");
+})

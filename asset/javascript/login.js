d7c1b07572cd46689623b5ec310a8c8569678e31
congMinh <minh.ptit.k6@gmail.com>
2025-07-12 03:56:57 +0700

    sửa lại tên file

--- asset/javascript/login.js
+++ asset/javascript/login.js
@@ -18,6 +18,25 @@ window.addEventListener("scroll",()=>{
     }
 })
 
+// ---menu Two--
+const divBar=document.getElementById("barsMmenu")
+const closeBar=document.getElementById("closeMenu")
+const overlayBar=document.getElementById("overlayBars")
+
+divBar.addEventListener("click",()=>{
+    document.getElementById("menuBars").style.transform="translateX(0px)"
+    document.getElementById("overlayBars").classList.add("hidden-overlay")
+})
+closeBar.addEventListener("click",()=>{
+    document.getElementById("menuBars").style.transform="translateX(-500px)"
+    document.getElementById("overlayBars").classList.remove("hidden-overlay")
+})
+overlayBar.addEventListener("click",()=>{
+    document.getElementById("menuBars").style.transform="translateX(-500px)"
+    document.getElementById("overlayBars").classList.remove("hidden-overlay")
+})
+
+
 document.querySelector("#signUp").addEventListener("click",()=>{
     document.querySelector(".box-sign-up").style.display="flex";
     document.querySelector(".box-sign-in").style.display="none";
